fa2b81683d6e6965fbd447f03b990257a19df404
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-07 14:01:31 +0100

    test commit

---
+++ target/debug/deps/minigrep-5a96cdefe4bac556.d
@@ -0,0 +1,5 @@
+/home/kkaramch/rust-projects/minigrep/target/debug/deps/minigrep-5a96cdefe4bac556.d: src/main.rs
+
+/home/kkaramch/rust-projects/minigrep/target/debug/deps/libminigrep-5a96cdefe4bac556.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/minigrep-992c06e7a41656bd.d
@@ -0,0 +1,5 @@
+/home/kkaramch/rust-projects/minigrep/target/debug/deps/minigrep-992c06e7a41656bd.d: src/main.rs
+
+/home/kkaramch/rust-projects/minigrep/target/debug/deps/libminigrep-992c06e7a41656bd.rmeta: src/main.rs
+
+src/main.rs:
