ee67d91ed90438a1b17a3ef219abf594394006bf
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-09 08:24:26 +0100

    added case sensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
 pub struct Config {
     pub query: String,
-    pub filename: String
+    pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -14,8 +16,9 @@ impl Config {
         }
         let query = args[1].clone(); 
         let filename = args[2].clone();
+        let case_sensitive = env::var("CASE_SENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -32,7 +35,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     
     file.read_to_string(&mut contents)?;
 
-    let result = search(&config.query, &contents);
+
+    let result = if config.case_sensitive {
+        search_case_sensitive(&config.query, &contents);
+    }
+    else {
+        search_case_insensitive(&config.query, &contents);
+    };
 
     println!("{:?}", result);
 
@@ -40,18 +49,33 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn search <'a>(q: &str, contents: &'a str) -> Vec<&'a str> {
+fn search_case_sensitive <'a>(q: &str, contents: &'a str) -> Vec<&'a str> {
 
     let mut result = Vec::new();
 
     for l in contents.lines() {
-        if l.contains(q){
-            result.push(l);
+        if l.contains(&q){
+            result.push(l.trim());
         }
     }
 
     result
 }
+
+
+fn search_case_insensitive <'a>(q: &str, contents: &'a str) -> Vec<&'a str> {
+
+    let mut result = Vec::new();
+
+    for l in contents.lines() {
+        if l.to_lowercase().contains(&q.to_lowercase()){
+            result.push(l.trim());
+        }
+    }
+
+    result
+}
+
 
 #[cfg(test)]
 mod test {
@@ -61,12 +85,41 @@ mod test {
     fn one_result(){
         let query = "biblioteca";
         let contents = "\
+        Text:
             donde esta la biblioteca?
             Mi amo T-bone
             La aragna discoteca
         ";
     
-        assert_eq!(vec!["donde esta la biblioteca?"], search(query, contents));
+        assert_eq!(vec!["donde esta la biblioteca?"], search_case_insensitive(query, contents));
 
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "Biblioteca";
+        let contents = "\
+        Text:
+            donde esta la biblioteca?
+            Mi amo T-bone
+            La aragna discoteca
+        ";
+    
+        assert_eq!(vec!["donde esta la biblioteca?"], search_case_insensitive(query, contents));
+    }
+
+
+    #[test]
+    fn case_sensitive(){
+        let query = "t-bone";
+        let contents = "\
+        Text:
+            donde esta la biblioteca?
+            Mi amo T-bone
+            La aragna discoteca
+            t-bone test
+        ";
+    
+        assert_eq!(vec!["t-bone test"], search_case_sensitive(query, contents));
     }
 }
