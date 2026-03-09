628bbd2eb7bb8daf280deab6ba7ba09050e99ccd
kushal karamchandani <kushal.karamchandani@nordakademie.de>
2026-03-09 09:15:11 +0100

    using eprintln for errors

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("error encountered while parsing args: {}", err);
+        eprintln!("error encountered while parsing args: {}", err);
         process::exit(1)
     });
     
     if let Err(e) = run(config){
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
